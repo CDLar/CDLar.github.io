4763dbc4e4180b8b7c05d68dae8e1d05e44aa9ec
CDLarsen <curtis.larssen@gmail.com>
2018-11-16 22:02:44 +0900

    1

--- SoupScraper.py
+++ SoupScraper.py
@@ -4,18 +4,8 @@ import bs4
 from bs4 import BeautifulSoup
 
 
-req = requests.get('http://www.espn.com/nhl/scoreboard?date=20181102')
+req = requests.get('http://fantasy.espn.com/hockey/league/scoreboard?leagueId=43185')
 soup = bs4.BeautifulSoup(req.text, 'lxml')
-scores = []
-names = []
-for i in soup.select('.team-score'):
-    scores.append(i.text)
 
-for i in soup.select('.team-name'):
-    names.append(i.text)    
-
-newNames = [names[0],names[2],names[6],names[8],names[12],names[14]]
-
-final = zip(newNames,scores)
-
-print(list(final))
+Score = soup.find_all('div',class_='ScoreCell__Score')
+print(Score)
