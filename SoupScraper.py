f144f9cbe36b673af67f2c7a3cab963f5d66dd60
CDLar <curtis.larssen@gmail.com>
2018-12-04 22:34:50 +0900

    1

--- SoupScraper.py
+++ SoupScraper.py
@@ -1,11 +1,1 @@
-# import libraries
-import requests
-import bs4
-from bs4 import BeautifulSoup
-
-
-req = requests.get('http://fantasy.espn.com/hockey/league/scoreboard?leagueId=43185')
-soup = bs4.BeautifulSoup(req.text, 'lxml')
-
-Score = soup.find_all('div',class_='ScoreCell__Score')
-print(Score)
+print('hello')
