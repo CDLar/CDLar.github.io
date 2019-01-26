c5bf963f590976b310fef626116ed3ee225be02c
CDLar <curtis.larssen@gmail.com>
2019-01-26 18:37:20 +0900

    2

---
+++ TeamScores.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Test</title>
+</head>
+
+<body>
+<?php 
+
+print"Hello";
+
+$connection=mysqli_connect("server187.web-hosting.com
+","bcfhckzf","Osiris321","bcfhckzf_MatchUps")
+
+if ($connection) {
+    echo "Connection established! <br>";
+} else  
+    die("COnnection failed. Reason: ".mysqli_connect_error());
+}
+?>
+
+
+</body>
+
+</html>
