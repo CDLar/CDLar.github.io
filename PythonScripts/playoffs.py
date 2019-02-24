8d75dab2b9a275b12700f128d5be5aa8503b3198
CDLar <curtis.larssen@gmail.com>
2019-02-24 15:55:14 +0900

    1

---
+++ PythonScripts/playoffs.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import pandas as pd
+app = Flask(__name__)
+
+scores = pd.read_csv("Weeklyscores.csv")
+
+@app.route('/')
+def index ():
+    return render_template("playoffs.html", name1 = scores.iat[0,1], s1 = scores.iat[0,2], name2 = scores.iat[1,1], s2= scores.iat[1,2])
+
+if __name__ == "__playoffs__":
+    app.run(debug=True)
+    
