c50c747956a7b601292af9fda7dbdda30d138203
CDLarsen <curtis.larssen@gmail.com>
2019-10-15 01:11:41 +0900

    1

--- Top10/Top10Template.py
+++ Top10/Top10Template.py
@@ -6,19 +6,6 @@ data = pd.read_csv('C:\\Users\\Curtis\\OneDrive\\Documents\\GitHub\\BCFHL\\Top10
 newData = data.drop(['Rk','Opponent', 'FP/G', '% Owned', '+/-'], axis=1)
 newData['Gp'] = data['FPts'].divide(data['FP/G'], fill_value=1) 
 newData['Gp'] = newData['Gp'].round(0)
-newData.drop(newData.index[16:], inplace=True)
-newData.Gp = newData.Gp.astype(int)
-
-print(newData)
-
-import pandas as pd
-import csv
-
-data = pd.read_csv('2019-week1.csv')
-
-newData = data.drop(['Rk','Opponent', 'FP/G', '% Owned', '+/-'], axis=1)
-newData['Gp'] = data['FPts'].divide(data['FP/G'], fill_value=1) 
-newData['Gp'] = newData['Gp'].round(0)
 topData = newData.drop(newData.index[10:])
 
 for index, row in topData.iterrows():
