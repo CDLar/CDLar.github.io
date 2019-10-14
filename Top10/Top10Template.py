a00991110230b28b92c92f6c27c5cb7dc16dff89
CDLarsen <curtis.larssen@gmail.com>
2019-10-15 02:03:02 +0900

    1

--- Top10/Top10Template.py
+++ Top10/Top10Template.py
@@ -9,6 +9,7 @@ newData['Gp'] = data['FPts'].divide(data['FP/G'], fill_value=1)
 newData['Gp'] = newData['Gp'].round(0)
 topData = newData.drop(newData.index[10:])
 
+print(topData)
 for index, row in topData.iterrows():
     x = index+1
     print(x,'. ', row['Player'],' (', row['Team'],' ',row['Position'],' - ',row['FPts'],' fpts - ','GP: ',row['Gp'], ') - Team ()',sep='')
